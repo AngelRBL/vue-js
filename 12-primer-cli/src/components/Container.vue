4dc8d87a1d3e1d3fcea77ab0c6fcbc36714cbe04
Angel Robles <69372927+AngelRBL@users.noreply.github.com>
2021-04-07 20:16:47 -0600

    spiner Loading agregado

--- 12-primer-cli/src/components/Container.vue
+++ 12-primer-cli/src/components/Container.vue
@@ -1,18 +1,38 @@
 <template>
   <div id="container" class="container">
     <h2>Proyectos</h2>
+    <img
+      src="https://avatars.githubusercontent.com/u/69372927?v=4"
+      alt="Avatar de Angel Robles"
+      width="100"
+      loading="lazy"
+      class="image"
+    />
     <hr />
-    <div class="cards">
-      <Card />
+    <loading v-if="load"/>
+    <div class="cards" v-for="project in projects" :key="project.id">
+      <Card
+        :name="project.name"
+        :description="project.description"
+        :author="project.owner.login"
+      />
     </div>
   </div>
 </template>
 
 <script>
 import Card from "./Card.vue";
+import Loading from './Loading'
 export default {
+  data() {
+    return {
+      projects: null,
+      load: false,
+    };
+  },
   components: {
     Card,
+    Loading,
   },
 
   mounted() {
@@ -21,15 +41,23 @@ export default {
 
   methods: {
     async getProjects() {
-      const res = await fetch("https://api.github.com/users/AngelRBL/repos")
-      const data = await res.json()
-      console.log(data)
-    }
+      this.load = true;
+      const res = await fetch("https://api.github.com/users/AngelRBL/repos");
+      const data = await res.json();
+
+      this.projects = data;
+      this.load = false;
+
+    },
   },
 };
 </script>
 
 <style scoped>
+.image {
+  border-radius: 50%;
+}
+
 .cards {
   display: flex;
   flex-wrap: wrap;
